cc7b85169108e37a01baeea50b9fff0396104847
karthik1521 <rskarthik1998@gmail.com>
2024-11-09 19:38:12 +0530

    Delete useraccessmanagementsystem project directory

--- useraccessmanagementsystem project/Back-end/src/main/java/com/uams/repo/SoftwareRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uams.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uams.entity.Software;
-
-public interface SoftwareRepository extends JpaRepository<Software, Long> {
-
-}
